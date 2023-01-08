49551d9ba3cf9d3bb14325a1def21fb51b32b3ad
Grigor Bezirganyan <grigor.bezirganyan98@gmail.com>
2023-01-08 17:38:54 +0100

    Bugs fixed in dataset generation, the labels are fixed to 23 classes

--- datasets/make_mmimdb.py
+++ datasets/make_mmimdb.py
@@ -4,11 +4,11 @@ import json
 # import logging
 import math
 import os
-import numpy
 import re
 import sys
 from collections import OrderedDict, Counter
 
+import numpy as np
 from PIL import Image
 from PIL.Image import Resampling
 # from fuel.datasets import H5PYDataset
@@ -50,7 +50,7 @@ def resize_and_crop_image(input_file, output_box=(224, 224), fit=True):
 
     # Resize the image with best quality algorithm ANTI-ALIAS
     img = img.resize(box, Resampling.LANCZOS).convert('RGB')
-    img = numpy.array(img)
+    img = np.array(img)
     # img[:, :, 0] -= 103.939
     # img[:, :, 1] -= 116.779
     # img[:, :, 2] -= 123.68
@@ -100,7 +100,7 @@ for i, filename in tqdm(enumerate(files), total=len(files)):
         #    data['plot'].append(data['plot outline'])
         im_file = file.replace('json', 'jpeg')
         if all([k in data for k in ('genres', 'plot')] + [os.path.isfile(im_file)]):
-            plot_id = numpy.array([len(p) for p in data['plot']]).argmax()
+            plot_id = np.array([len(p) for p in data['plot']]).argmax()
             data['plot'] = normalizeText(data['plot'][plot_id])
             if len(data['plot']) > 0:
                 vocab_counts.extend(data['plot'])
@@ -122,10 +122,10 @@ target_names = list(counts.keys())[:n_classes]
 
 le = MultiLabelBinarizer()
 Y = le.fit_transform([m['genres'] for m in movies])
-labels = numpy.nonzero(le.transform([[t] for t in target_names]))[1]
+labels = np.nonzero(le.transform([[t] for t in target_names]))[1]
 
-B = numpy.copy(Y)
-rng = numpy.random.RandomState(rng_seed)
+B = np.copy(Y)
+rng = np.random.RandomState(rng_seed)
 train_idx, dev_idx, test_idx = [], [], []
 for l in labels[::-1]:
     t = B[:, l].nonzero()[0]
@@ -138,7 +138,7 @@ for l in labels[::-1]:
     train_idx.extend(t[n_test + n_dev:])
     B[t, :] = 0
 
-indices = numpy.concatenate([train_idx, dev_idx, test_idx])
+indices = np.concatenate([train_idx, dev_idx, test_idx])
 nsamples = len(indices)
 nsamples_train, nsamples_dev, nsamples_test = len(
     train_idx), len(dev_idx), len(test_idx)
@@ -158,13 +158,16 @@ output_dir = '../output'
 split_dict = {
     'train': {
         'text': (0, nsamples_train),
-        'images': (0, nsamples_train)},
+        'images': (0, nsamples_train),
+        'labels': (0, nsamples_train)},
     'dev': {
         'text': (nsamples_train, nsamples_train + nsamples_dev),
-        'images': (nsamples_train, nsamples_train + nsamples_dev)},
+        'images': (nsamples_train, nsamples_train + nsamples_dev),
+        'labels': (nsamples_train, nsamples_train + nsamples_dev)},
     'test': {
         'text': (nsamples_train + nsamples_dev, nsamples),
-        'images': (nsamples_train + nsamples_dev, nsamples)}
+        'images': (nsamples_train + nsamples_dev, nsamples),
+        'labels': (nsamples_train + nsamples_dev, nsamples)}
 }
 
 print('Saving data...')
@@ -178,29 +181,27 @@ for set in split_dict:
             os.makedirs(f'{output_dir}/{set}/{modality}')
 
     print(' - Saving text data...')
-    for idx in tqdm(range(split_dict[set]['text'][0], split_dict[set]['text'][1])):
-        with open(f'{output_dir}/{set}/text/{movies[idx]["imdb_id"]}.txt', 'w') as f:
+    for i, idx in tqdm(enumerate(range(split_dict[set]['text'][0], split_dict[set]['text'][1]))):
+        with open(f'{output_dir}/{set}/text/text_{i}.txt', 'w') as f:
             f.write(' '.join(X[idx]))
     print(' - Saving image data...')
-    for idx in tqdm(range(split_dict[set]['images'][0], split_dict[set]['images'][1])):
+    for i, idx in tqdm(enumerate(range(split_dict[set]['images'][0], split_dict[set]['images'][1]))):
         image = movies[idx]['cover'].T
-        plt.imsave(f'{output_dir}/{set}/images/{movies[idx]["imdb_id"]}.jpeg', image.T)
+        plt.imsave(f'{output_dir}/{set}/images/image_{i}.jpeg', image.T)
 
     print(' - Saving labels...')
-    with open(f'{output_dir}/{set}/labels.csv', 'w') as f:
-        f.write('imdb_id,genre')
-        for idx in tqdm(range(split_dict[set]['images'][0], split_dict[set]['images'][1])):
-            f.write(f'\n{movies[idx]["imdb_id"]},{Y[idx]}')
+    for i, idx in tqdm(enumerate(range(split_dict[set]['labels'][0], split_dict[set]['labels'][1]))):
+        np.save(f'{output_dir}/{set}/labels/label_{i}.npy', Y[idx][labels])
 
 
 # Plot distribution
 # print('Plotting distribution...')
-# cm = numpy.zeros((n_classes, n_classes), dtype='int')
+# cm = np.zeros((n_classes, n_classes), dtype='int')
 # for i, l in enumerate(labels):
 #     cm[i] = Y[Y[:, l].nonzero()[0]].sum(axis=0)[labels]
 #
 # cmap = plt.cm.Blues
-# cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, numpy.newaxis]
+# cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 # for i in range(len(target_names)):
 #     cm_normalized[i, i] = 0
 # plt.imshow(cm_normalized, interpolation='nearest', cmap=cmap, aspect='auto')
@@ -210,7 +211,7 @@ for set in split_dict:
 #             plt.text(j - .2, i + .2, c, fontsize=4)
 # plt.title('Shared labels', fontsize='smaller')
 # plt.colorbar()
-# tick_marks = numpy.arange(len(target_names))
+# tick_marks = np.arange(len(target_names))
 # plt.xticks(tick_marks, target_names, rotation=90)
 # plt.yticks(tick_marks, target_names)
 # plt.tight_layout()
