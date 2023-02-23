59921407e31c51efa295e34f2ff7ef8522b3c880
Grigor Bezirganyan <grigor.bezirganyan98@gmail.com>
2023-02-23 12:13:36 +0100

    Added concat fusion for DynaMixer

--- modules/fusion.py
+++ modules/fusion.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 from torch import nn
 
@@ -19,6 +21,60 @@ class BiModalGatedUnit(nn.Module):
         z = torch.sigmoid(z_hidden)
 
         return z * mod1_hidden + (1 - z) * mod2_hidden
+
+    def get_output_shape(self, *args, dim=None):
+        """
+        Returns the output shape of the layer given the input shape.
+
+        Parameters
+        ----------
+        *args : tuple, list, torch.Size, int
+            The input shape of the layer. If a tuple, list, or torch.Size, then the full shape is expected. If an int,
+            then the dimension parameter is also expected, and the result will be the output shape of that dimension.
+        dim : int, optional
+            The dimension of the input shape. Only used if the first argument is an int. Defaults to None. If not None,
+            then the args argument is expected to be an int and match the input shape of at the given dimension.
+            then the result will be the output shape of that dimension. Since this fusion performs the transformation
+            on the last dimension, to get the shape of the last dimension, set dim to -1. Otherwise, the output value
+            will be equal to the input value.
+
+        Returns
+        -------
+        tuple, int
+
+        """
+        if dim is not None:
+            if not isinstance(args[0], int):
+                raise ValueError("The dim argument is only used if the first argument is an int.")
+            if dim == -1:
+                return self.out_size
+            else:
+                return args[0][dim] if isinstance(args[0], list | tuple | torch.Size) else args[0]
+        shape1 = list(args[0])
+        shape1[-1] = self.out_size
+        return tuple(shape1)
+
+
+class MultiModalGatedUnit(nn.Module):
+    def __init__(self, n_modalities, in_shape, out_size=None, **kwargs):
+        super(MultiModalGatedUnit, self).__init__()
+        if out_size is None:
+            out_size = in_shape
+        self.out_size = out_size
+        self.modalities = nn.ModuleList([])
+        self.z_list = nn.ModuleList([])
+        for mod in n_modalities:
+            self.modalities.append(nn.Linear(mod, out_size))
+            self.z_list.append(nn.Linear(in_shape * n_modalities, out_size))
+
+    def forward(self, *args):
+        out = torch.zeros((*args[0].shape[:-1], self.out_size))
+        for mod, z_enc, arg in zip(self.modalities, self.z_list, args):
+            h = torch.tanh(mod(arg))
+            z = torch.sigmoid(z_enc(torch.cat(args, dim=-1)))
+            out += z * h
+
+        return out
 
     def get_output_shape(self, *args, dim=None):
         """
@@ -89,6 +145,46 @@ class ConcatFusion:
             shape[self.dim] += arg[self.dim]
         return tuple(shape)
 
+
+class ConcatDynaFusion:
+    def __init__(self, dim=1, **kwargs):
+        self.dim = dim
+
+    def __call__(self, *args):
+        a = torch.cat(args, dim=1)
+        b = torch.cat([a, a], dim=2)
+
+        return b
+
+    def get_output_shape(self, *args, dim=None):
+        """
+        Returns the output shape of the layer given the input shape.
+        Parameters
+        ----------
+        *args : tuple, list, torch.Size, int
+            The input shape of the layer. If a tuple, list, or torch.Size, then the full shape is expected. If an int,
+            then the dimension parameter is also expected, and the result will be the output shape of that dimension.
+        dim : int, optional
+            The dimension of the input shape. Only used if the first argument is an int. Defaults to None. If not None,
+            then the args argument is expected to be an int and match the input shape of at the given dimension.
+
+        Returns
+        -------
+        tuple, int
+
+        """
+        if dim is not None:
+            if not isinstance(args[0], int):
+                raise ValueError("The dim argument is only used if the first argument is an int.")
+            if dim == self.dim:
+                return (int(math.sqrt(args[0])) * 2)**2
+            else:
+                return args[0]
+        shape = list(args[0])
+        for arg in args[1:]:
+            shape[1] += arg[1]
+            shape[2] += arg[2]
+        return tuple(shape)
 
 class MaxFusion:
     def __init__(self, **kwargs):
--- tests/modules/test_fusion.py
+++ tests/modules/test_fusion.py
@@ -1,7 +1,8 @@
 import pytest
 import torch
 
-from modules import ConcatFusion, SumFusion, MaxFusion, MeanFusion, BiModalGatedUnit
+from modules import ConcatFusion, SumFusion, MaxFusion, MeanFusion, BiModalGatedUnit, MultiModalGatedUnit, \
+    ConcatDynaFusion
 
 
 class TestFusions:
@@ -18,6 +19,18 @@ class TestFusions:
 
         assert fusion.get_output_shape(input_1.shape, input_2.shape) == (10, 40, 30)
         assert fusion.get_output_shape(20, 20, dim=1) == 40
+        assert fusion.get_output_shape(20, 20, dim=0) == 20
+        with pytest.raises(ValueError):
+            fusion.get_output_shape(input_1, input_2, dim=2)
+
+    def test_concat_dyna_fusion(self):
+        fusion = ConcatDynaFusion(useless_arg=1)
+        input_1 = torch.rand(10, 20, 20, 30)
+        input_2 = torch.rand(10, 20, 20, 30)
+        assert fusion(input_1, input_2).shape == (10, 40, 40, 30)
+
+        assert fusion.get_output_shape(input_1.shape, input_2.shape) == (10, 40, 40, 30)
+        assert fusion.get_output_shape(36, 36, dim=1) == 12*12
         assert fusion.get_output_shape(20, 20, dim=0) == 20
         with pytest.raises(ValueError):
             fusion.get_output_shape(input_1, input_2, dim=2)
@@ -65,3 +78,15 @@ class TestFusions:
         assert fusion.get_output_shape(20, 20, dim=-1) == 30
         with pytest.raises(ValueError):
             fusion.get_output_shape(input_1, input_2, dim=2)
+
+    def test_multimodal_gu_fusion(self):
+        input_1 = torch.rand(10, 20, 30)
+        input_2 = torch.rand(10, 20, 30)
+        input_3 = torch.rand(10, 20, 30)
+        fusion = MultiModalGatedUnit(30, 30, 30, out_size=30, useless_arg=1)
+        assert fusion(input_1, input_2, input_3).shape == (10, 20, 30)
+        assert fusion.get_output_shape(input_1.shape, input_2.shape) == (10, 20, 30)
+        assert fusion.get_output_shape(20, 20, 20, dim=1) == 20
+        assert fusion.get_output_shape(20, 20, 20, dim=-1) == 30
+        with pytest.raises(ValueError):
+            fusion.get_output_shape(input_1, input_2, input_3, dim=2)
